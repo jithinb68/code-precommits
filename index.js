07e06170fbf324706885e2f47fd1c361947ec201
Jithin <jithin@Jithins-MacBook-Pro.local>
2024-12-27 18:52:51 +0530

    prettier test 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const sayHello = () => {
+  console.log('helllo')
+}
+
+const loopMe = () => {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index]
+  }
+}
+
+const obj = {
+  name: 'John',
+  age: 30,
+}
